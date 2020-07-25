a52faa9b4cf6ff369ca94f469baaa756b519dea4
Tom Fischer <tgfischer6@gmail.com>
2020-07-25 08:32:47 -0400

    feat: Add support for radio component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -8,6 +8,9 @@ import { Input } from "./Input";
 import { Select } from "./Select";
 import { Textarea } from "./Textarea";
 import { Checkbox, CheckboxGroup } from "./Checkbox";
+import { Radio, RadioGroup } from "./Radio";
+
+export { useFormikContext, useField } from "formik";
 
 type FormComponent = FC<FormikConfig<FormikValues>> & {
   Group: typeof Group;
@@ -16,6 +19,8 @@ type FormComponent = FC<FormikConfig<FormikValues>> & {
   Select: typeof Select;
   CheckboxGroup: typeof CheckboxGroup;
   Checkbox: typeof Checkbox;
+  RadioGroup: typeof RadioGroup;
+  Radio: typeof Radio;
 };
 
 export const Form: FormComponent = ({
@@ -35,3 +40,5 @@ Form.Textarea = Textarea;
 Form.Select = Select;
 Form.CheckboxGroup = CheckboxGroup;
 Form.Checkbox = Checkbox;
+Form.RadioGroup = RadioGroup;
+Form.Radio = Radio;
