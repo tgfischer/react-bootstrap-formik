1f4cbab3e2e34b0200e01ee7f64793223c9d653e
Tom Fischer <tgfischer6@gmail.com>
2021-01-09 10:34:58 -0500

    fix: upgraded npm dependencies to fix audit issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/__stories__/*.stories.(ts|tsx)"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links", "@storybook/addon-docs"],
+  stories: ["../src/**/__stories__/*.stories.@(ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+  typescript: {
+    check: true,
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false
+      }
+    }
+  },
   webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("ts-loader"),
-          options: {
-            transpileOnly: true,
-          },
-        },
-        {
-          loader: require.resolve("react-docgen-typescript-loader"),
-        },
-      ],
-    });
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../"),
     });
-    config.resolve.extensions.push(".ts", ".tsx");
     return config;
   },
 };
